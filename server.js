236d7fc4ca7cc619e4420d743452bdeb96d3ea90
boc4life <lheandrew@gmail.com>
2019-03-29 15:57:46 -0400

    fixed up server.js

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-var db = require("./models");
-
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
